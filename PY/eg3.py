e8c296f578f8e6ac571ecd5f21efa373351f0947
AmanByte1 <amandiwan775@gmail.com>
2026-08-27 23:37:43 +0530

    hmm..

---
+++ PY/eg3.py
@@ -0,0 +1,13 @@
+def i(arr,t):
+    l=0
+    i1=0
+    while l<len(arr):
+        # i1=0
+        i2=0
+        for i in arr:
+            if arr[l]+i==t:
+                return i1,i2
+            i2+=1
+        i1+=1
+        l+=1
+print(i([1,2,3,4,5],7))
