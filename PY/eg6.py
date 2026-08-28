91b92e3dfacfb9c2efd9275251059d64cfa4abb1
AmanByte1 <amandiwan775@gmail.com>
2026-08-28 22:18:17 +0530

    hmm..

---
+++ PY/eg6.py
@@ -0,0 +1,12 @@
+def rf(fn):
+    with open(fn,'r') as file:
+        return file.readlines()
+
+def rf1(fn):
+    with open(fn,'r') as file:
+        for i in file:
+            yield i
+
+
+for l in rf("xyz.txt"):
+    process(l)
