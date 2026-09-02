421edbd025444c0444fd1c7b0deea3745f77924f
AmanByte1 <amandiwan775@gmail.com>
2026-09-02 18:05:10 +0530

    p4

--- nodeP/fs.js
+++ nodeP/fs.js
@@ -3,5 +3,13 @@ const fs=require("fs");
 fs.readFile('file.txt','utf-8',(err,data)=>{
     console.log(err,data)
     console.log(data)
-    
+
+})
+
+fs.writeFile('file.txt',"This is a file which contain info",()=>{
+    console.log("done")
+
+})
+fs.appendFile("file.txt"," hello",()=>{
+    console.log("perfecto")
 })
